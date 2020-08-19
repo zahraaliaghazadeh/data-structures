0d456dc2ad81e65036e083a108a3aeb6599bf727
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-08-19 11:54:46 -0700

    added the bubble sort with no swap

--- BubbleSort/index.js
+++ BubbleSort/index.js
@@ -47,19 +47,46 @@
 
 
 // ES2015 using arr[idx] etc
+// function bubbleSort(arr){
+//     const swap = (arr,idx1,idx2) =>{
+//         [arr[idx1],arr[idx2]] = [arr[idx2],arr[idx1]];
+//     };
+//     for(let i = arr.length ; i>0 ; i--){
+//         for (let j=0 ; j<i-1 ;j++){
+//             if(arr[j]>arr[j+1]){
+//                 swap(arr,j,j+1);
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(bubbleSort([37,45,29,8]))
+
+
+// optimized with No Swaps
 function bubbleSort(arr){
-    const swap = (arr,idx1,idx2) =>{
-        [arr[idx1],arr[idx2]] = [arr[idx2],arr[idx1]];
-    };
-    for(let i = arr.length ; i>0 ; i--){
-        for (let j=0 ; j<i-1 ;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr,j,j+1);
-            }
+    var noSwaps;
+    for(var i=arr.length ; i>0 ; i--){
+        noSwaps =true;
+        for ( var j=0 ; j<i-1 ; j++){
+              console.log(arr,arr[j],arr[j+1])
+
+           if  (arr[j] > arr[j+1]){
+            //    Swap!
+            var temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+            noSwaps=false;
+           }
         }
+        // console.log("One Pass complete!")
+        if(noSwaps) break;
     }
-    return arr;
+    return arr
 }
 
 console.log(bubbleSort([37,45,29,8]))
 
+// big O is O(n^2) but for nearly sorted data is O(n) almost with the optimized version no Swaps
+// to short circuit if there is no swap
