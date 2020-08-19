a3caace6970ea7afc7853b167faf59de3cabd39c
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-08-19 12:44:16 -0700

    insertion sort

--- InsertionSort/index.js
+++ InsertionSort/index.js
@@ -6,3 +6,25 @@
 // Now compare the second element with the one before it and swap if necessary.
 // Continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct place.
 // Repeat until the array is sorted.
+
+// O(n^2) worst case
+// but if data is nearly sort
+
+// what is insertion sort good at?
+// online alg, as it receives new data, it is a good stength
+// it doesnt need the full array of data
+// data streaming in live
+
+function insertionSort(arr){
+    for(var i=1;i<arr.length;i++){
+var currentVal = arr[i];
+for ( var j=i-1 ; j>= 0 && arr[j]>currentVal; j--){
+    arr[j+1] = arr[j]
+}
+arr[j+1] = currentVal;
+console.log(arr)
+
+    }
+    return arr;
+}
+console.log(insertionSort([2,1,9,76,4]))
